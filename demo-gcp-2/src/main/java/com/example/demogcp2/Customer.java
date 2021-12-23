6296c7d5598974eb06751766a5e70b6c3ab6b696
Yogesh Patil <yogesh.hpatil4@gmail.com>
2021-12-23 13:54:44 +0530

    Create Customer.java

---
+++ demo-gcp-2/src/main/java/com/example/demogcp2/Customer.java
@@ -0,0 +1,36 @@
+package com.example.demogcp2;
+
+public class Customer {
+
+	private Long id;
+	private String name;
+	private String address;
+	private String dob;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	
+	
+}
